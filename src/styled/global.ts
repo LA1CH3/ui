aa14209cf8691becf5f6513b4a19d2fc0293fba5
Jay Laiche <jay@jaylaiche.com>
2021-08-17 21:26:53 -0400

    fix(styled): add box-sizing border-box to global styles

--- src/styled/global.ts
+++ src/styled/global.ts
@@ -5,6 +5,9 @@ import { global } from "@stitches/react";
 // stolen from https://github.com/hauptrolle/stitches-reset
 
 export const globalStyles = global({
+  "*": {
+    boxSizing: "border-box",
+  },
   "html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video":
     {
       margin: "0",
