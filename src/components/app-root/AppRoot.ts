dff92b9a1d0df2d1b532111766ce50210eb32f7c
Jay Laiche <jay@jaylaiche.com>
2021-08-18 21:24:34 -0400

    fix(components): set AppRoot to use min-height

--- src/components/app-root/AppRoot.ts
+++ src/components/app-root/AppRoot.ts
@@ -3,7 +3,7 @@ import { styled } from "../../styled";
 export const AppRoot = styled("div", {
   position: "absolute",
   width: "100%",
-  height: "100%",
+  minHeight: "100%",
   display: "flex",
   flexDirection: "column",
   backgroundColor: "$textPrimaryInverted",
